f8aa4e4fcf1769e1a80df2d41720152f09d4b19f
vedali29 <vedalipawar29@gmail.com>
2024-08-31 17:59:26 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(
-            process.env.MONGO_URL,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
+            process.env.MONGO_URI,
         );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    resume: String,
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resume: { type: String }, // Resume file name/path
 });
 
 const User = mongoose.model('User', userSchema);
